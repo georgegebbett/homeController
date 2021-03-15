4f75b726ed1859de54f062e927546bb4b4913738
George Gebbett <george.gebbett1@gmail.com>
2021-03-15 20:19:35 +0000

    let try thid

--- main2.py
+++ main2.py
@@ -71,16 +71,17 @@ class LightsPage(tk.Frame):
         i = 0
         print(lightGroups)
         for lightGroup in lightGroups:
-            if 'Group for' in lightGroup.values[0]:
-                continue
-            else:
-                button = tk.Button(self, text=lightGroup.values[0],
-                                   command=lambda: controller.show_frame(StartPage))
-                button.grid(column=i%2, row=int(i/2)+1, sticky="NSEW")
-                self.grid_columnconfigure(i%2, weight=1)
-                self.grid_rowconfigure(int(i/2)+1, weight=1)
-
-                i += 1
+            print(lightGroup)
+            # if 'Group for' in lightGroup.values[0]:
+            #     continue
+            # else:
+            #     button = tk.Button(self, text=lightGroup.values[0],
+            #                        command=lambda: controller.show_frame(StartPage))
+            #     button.grid(column=i%2, row=int(i/2)+1, sticky="NSEW")
+            #     self.grid_columnconfigure(i%2, weight=1)
+            #     self.grid_rowconfigure(int(i/2)+1, weight=1)
+            #
+            #     i += 1
 
         # button2 = tk.Button(self, text="Home",
         #                     command=lambda: controller.show_frame(StartPage))
