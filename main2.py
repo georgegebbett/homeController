417665d68c3526e2150d93318f982647e1f39357
George Gebbett <george.gebbett1@gmail.com>
2021-03-16 16:10:54 +0000

    no cursor

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import configparser
+import json
 from ast import literal_eval
 
 config = configparser.ConfigParser()
@@ -38,9 +39,12 @@ for device in tapoDevices:
 print('Tapo devices:')
 
 for device in tapoDeviceObjects:
-    print(getattr(device, 'name'),
+    print(device,
+          getattr(device, 'name'),
           getattr(device, 'room'),
           getattr(device, 'ipAddress'))
+    device.handshake()
+    device.login()
 
 fairyLights = PyP100.P100("192.168.1.104", tapoUser, tapoPass)
 fairyLights.handshake()
@@ -66,7 +70,7 @@ class HomeController(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, RoomsPage, PresetsPage, RoomControlPage):
+        for F in (StartPage, RoomsPage, PresetsPage, RoomControlPage, TapoControlPage):
             frame = F(container, self)
             self.frames[F] = frame
 
@@ -111,7 +115,7 @@ class RoomsPage(tk.Frame):
         i = 0
         for room in rooms:
             button = tk.Button(self, text=room['name'], font=LARGE_FONT, wraplength='140',
-                               command=lambda roomId=room['id']: openLightControlPage(roomId, controller))
+                               command=lambda roomId=room['id']: openRoomControlPage(roomId, controller))
             setattr(button, 'id', room['id'])
             button.grid(column=i % 2, row=int(i / 2) + 1, sticky="NSEW")
             self.grid_columnconfigure(i % 2, weight=1)
@@ -135,34 +139,97 @@ class RoomControlPage(tk.Frame):
         self.grid_rowconfigure(2, weight=1)
         self.grid_rowconfigure(3, weight=1)
         self.roomToBeControlledName = tk.StringVar()
+        self.lightButtonText = tk.StringVar()
         print(self)
 
         label = tk.Label(self, textvariable=self.roomToBeControlledName, font=LARGE_FONT)
         label.grid(column=0, row=0, sticky='EW', columnspan=2)
 
-        button = tk.Button(self, text="Lights ON", font=LARGE_FONT,
-                           command=lambda: lightStateChange(True))
+        button = tk.Button(self, textvariable=self.lightButtonText, wraplength='140', font=LARGE_FONT,
+                           command=lambda: lightStateChange())
         button.grid(column=0, row=1, sticky="NSEW")
 
-        button2 = tk.Button(self, text="Lights OFF", font=LARGE_FONT,
+        button2 = tk.Button(self, text="Scenes", font=LARGE_FONT, wraplength='140',
                             command=lambda: lightStateChange(False))
         button2.grid(column=1, row=1, sticky="NSEW")
 
-        button3 = tk.Button(self, text="Flights On", font=LARGE_FONT,
-                            command=lambda: fairyLights.turnOn())
+        button3 = tk.Button(self, text="Smart Plugs", font=LARGE_FONT, wraplength='140',
+                            command=lambda: openTapoControlPage(controller))
         button3.grid(column=0, row=2, sticky="NSEW")
 
-        button4 = tk.Button(self, text="Flights Off", font=LARGE_FONT,
+        button4 = tk.Button(self, text="Music", font=LARGE_FONT, wraplength='140',
                             command=lambda: fairyLights.turnOff())
         button4.grid(column=1, row=2, sticky="NSEW")
 
-        button5 = tk.Button(self, text="Back", font=LARGE_FONT,
+        button5 = tk.Button(self, text="Back", font=LARGE_FONT, wraplength='140',
                             command=lambda: controller.show_frame(RoomsPage))
         button5.grid(column=0, row=3, sticky="NSEW", columnspan=2)
 
     def updateName(self, roomID):
         self.roomToBeControlledName.set(rooms[roomID]['name'])
 
+    def getLightState(self, roomID):
+        for room in rooms:
+            if room['id'] == roomID:
+                if b.get_group(room['hueGroup'], 'on'):
+                    print('lights on')
+                    self.lightButtonText.set("Lights Off")
+                else:
+                    print('lights off')
+                    self.lightButtonText.set("Lights On")
+
+
+class TapoControlPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.roomToBeControlledName = tk.StringVar()
+        self.lightButtonText = tk.StringVar()
+        print(self)
+
+        label = tk.Label(self, textvariable=self.roomToBeControlledName, font=LARGE_FONT)
+        label.grid(column=0, row=0, sticky='EW', columnspan=2)
+
+    def drawButtons(self, tapoList, controller):
+
+        if tapoList:
+            i = 0
+            for tapo in tapoList:
+                button = tk.Button(self, text=getattr(tapo, 'name'), font=LARGE_FONT, wraplength='140',
+                                   command=lambda: changeTapoState(tapo))
+                setattr(button, 'id', room['id'])
+                button.grid(column=i % 2, row=int(i / 2) + 1, sticky="NSEW")
+                self.grid_columnconfigure(i % 2, weight=1)
+                self.grid_rowconfigure(int(i / 2) + 1, weight=1)
+
+                i += 1
+
+            button5 = tk.Button(self, text="Back", font=LARGE_FONT, wraplength='140',
+                            command=lambda: controller.show_frame(RoomControlPage))
+            button5.grid(column=0, row=i + 1, sticky="NSEW", columnspan=2)
+        else:
+            button5 = tk.Button(self, text="Back", font=LARGE_FONT, wraplength='140',
+                                command=lambda: controller.show_frame(RoomControlPage))
+            button5.grid(column=0, row=3, sticky="SEW", columnspan=2)
+
+
+    def updateName(self, roomID):
+        self.roomToBeControlledName.set(rooms[roomID]['name'])
+
+    def getLightState(self, roomID):
+        for room in rooms:
+            if room['id'] == roomID:
+                if b.get_group(room['hueGroup'], 'on'):
+                    print('lights on')
+                    self.lightButtonText.set("Lights Off")
+                else:
+                    print('lights off')
+                    self.lightButtonText.set("Lights On")
 
 
 class PresetsPage(tk.Frame):
@@ -185,21 +252,43 @@ class PresetsPage(tk.Frame):
         button2.grid(column=0, row=2, sticky="NSEW")
 
 
-def openLightControlPage(room, controller):
+def openRoomControlPage(room, controller):
     global roomToBeControlled
     roomToBeControlled = room
-    print('light controls for', room)
+    print('room controls for', room)
     app.frames[RoomControlPage].updateName(roomToBeControlled)
+    app.frames[RoomControlPage].getLightState(roomToBeControlled)
     controller.show_frame(RoomControlPage)
 
 
-def lightStateChange(onBool):
+def openTapoControlPage(controller):
+    print('tapo controls for', roomToBeControlled)
+    app.frames[TapoControlPage].updateName(roomToBeControlled)
+    print("tapo devices in this room:")
+    roomTapos = []
+    for device in tapoDeviceObjects:
+        if getattr(device, 'room') == roomToBeControlled:
+            print(getattr(device, 'name'))
+            roomTapos.append(device)
+    controller.show_frame(TapoControlPage)
+    app.frames[TapoControlPage].drawButtons(roomTapos, controller)
+
+def changeTapoState(tapoObj):
+    # print(literal_eval(tapoObj.getDeviceInfo())['result']['device_on'])
+    if json.loads(tapoObj.getDeviceInfo())['result']['device_on']:
+        tapoObj.turnOff()
+    else:
+        tapoObj.turnOn()
+
+
+def lightStateChange():
     for room in rooms:
         if room['id'] == roomToBeControlled:
-            if onBool:
-                b.set_group(room['hueGroup'], 'on', True)
-            else:
+            if b.get_group(room['hueGroup'], 'on'):
                 b.set_group(room['hueGroup'], 'on', False)
+            else:
+                b.set_group(room['hueGroup'], 'on', True)
+    app.frames[RoomControlPage].getLightState(roomToBeControlled)
 
 
 app = HomeController()
