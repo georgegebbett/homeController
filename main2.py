2c260d9ace0af3a588a0e3aa6af8785b835ea538
George Gebbett <george.gebbett1@gmail.com>
2021-03-16 11:32:19 +0000

    big text

--- main2.py
+++ main2.py
@@ -68,7 +68,7 @@ class StartPage(tk.Frame):
                            command=lambda: controller.show_frame(LightsPage))
         button.grid(column=0, row=1, sticky="NSEW")
 
-        button2 = tk.Button(self, text="Presets",
+        button2 = tk.Button(self, text="Presets", font=LARGE_FONT,
                             command=lambda: controller.show_frame(PresetsPage))
         button2.grid(column=0, row=2, sticky="NSEW")
 
@@ -90,7 +90,7 @@ class LightsPage(tk.Frame):
             if 'Group for' in lightGroups[lightGroup]['name']:
                 continue
             else:
-                button = tk.Button(self, text=lightGroups[lightGroup]['name'],
+                button = tk.Button(self, text=lightGroups[lightGroup]['name'], font=LARGE_FONT,
                                    command=lambda: controller.show_frame(LightsControlPage))
                 button.grid(column=i%2, row=int(i/2)+1, sticky="NSEW")
                 self.grid_columnconfigure(i%2, weight=1)
@@ -98,7 +98,7 @@ class LightsPage(tk.Frame):
 
                 i += 1
 
-        button2 = tk.Button(self, text="Home",
+        button2 = tk.Button(self, text="Home", font=LARGE_FONT,
                             command=lambda: controller.show_frame(StartPage))
         button2.grid(column=0, row=i+1, sticky="NSEW", columnspan=2)
         self.grid_rowconfigure(i+1, weight=1)
@@ -117,23 +117,23 @@ class LightsControlPage(tk.Frame):
         label = tk.Label(self, text="Home Controller", font=LARGE_FONT)
         label.grid(column=0, row=0, sticky='EW', columnspan = 2)
 
-        button = tk.Button(self, text="ON",
+        button = tk.Button(self, text="ON", font=LARGE_FONT,
                            command=lambda: b.set_group(1,'on', True))
         button.grid(column=0, row=1, sticky="NSEW")
 
-        button2 = tk.Button(self, text="OFF",
+        button2 = tk.Button(self, text="OFF", font=LARGE_FONT,
                             command=lambda: b.set_group(1,'on', False))
         button2.grid(column=1, row=1, sticky="NSEW")
 
-        button3 = tk.Button(self, text="Flights On",
+        button3 = tk.Button(self, text="Flights On", font=LARGE_FONT,
                             command=lambda: fairyLights.turnOn())
         button3.grid(column=0, row=2, sticky="NSEW")
 
-        button4 = tk.Button(self, text="Flights Off",
+        button4 = tk.Button(self, text="Flights Off", font=LARGE_FONT,
                             command=lambda: fairyLights.turnOff())
         button4.grid(column=1, row=2, sticky="NSEW")
 
-        button5 = tk.Button(self, text="Back",
+        button5 = tk.Button(self, text="Back", font=LARGE_FONT,
                             command=lambda: controller.show_frame(LightsPage))
         button5.grid(column=0, row=3, sticky="NSEW", columnspan=2)
 
@@ -149,11 +149,11 @@ class PresetsPage(tk.Frame):
         label = tk.Label(self, text="Heating control", font=LARGE_FONT)
         label.grid(column=0, row=0, sticky='EW')
 
-        button = tk.Button(self, text="Heating on",
+        button = tk.Button(self, text="Heating on", font=LARGE_FONT,
                            command=lambda: controller.show_frame(StartPage))
         button.grid(column=0, row=1, sticky="NSEW")
 
-        button2 = tk.Button(self, text="Home",
+        button2 = tk.Button(self, text="Home", font=LARGE_FONT,
                             command=lambda: controller.show_frame(StartPage))
         button2.grid(column=0, row=2, sticky="NSEW")
 
